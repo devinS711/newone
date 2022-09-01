a776c743bc62241e78f1464252957277916614d1
devin <devin2014.ds@gmail.com>
2022-09-01 12:24:49 -0500

    Initial Commit

---
+++ Shared/newoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  newoneApp.swift
+//  Shared
+//
+//  Created by Devin Schupbach on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct newoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
